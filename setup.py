8466c088b90f922e251c94b5fd40d0418ce495e1
cjdinsmore <caleb.dinsmore@edusource.us>
2019-05-14 12:54:22 -0400

    Version 0.2.4: fixed bug with generated keyfile being used

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="data_anonymizer",
-    version="0.2.3",
+    version="0.2.4",
     author="Caleb Dinsmore",
     author_email="caleb.dinsmore@edusource.us",
     description="A tool to anonymize data within a CSV",
